38dea9debfecee1079961e3d716d4e5f879c2464
Spiros Raptis <1444577+spirosrap@users.noreply.github.com>
2026-06-23 22:01:18 +0300

    Fix Safari stylesheet loading

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const stripSameOriginCrossorigin = () => ({
+  name: "strip-same-origin-crossorigin",
+  enforce: "post",
+  transformIndexHtml(html) {
+    return html.replaceAll(" crossorigin", "");
+  },
+});
+
 export default defineConfig(({ command }) => ({
   base: command === "build" ? "/demo/posokanei-basket/" : "/",
   build: {
     target: ["es2019", "safari14"],
   },
-  plugins: [react()],
+  plugins: [react(), stripSameOriginCrossorigin()],
 }));
